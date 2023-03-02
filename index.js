cde4f6eb5b2e4fae890f3af9dca519cf40602935
MARIE Raphael <56415306+R-o-h-t@users.noreply.github.com>
2023-03-02 14:16:29 +0100

    react render

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
 const render = (structure, selector) => {
-    // TODO LOGIQUE
+  const element =
+    typeof selector === "string" ? document.querySelector(selector) : selector;
+
+  const { type, props, children } = structure;
+
+  // Création de l'élément
+  const newElement = document.createElement(type);
+
+  // Ajout des propriétés
+  Object.keys(props).forEach((prop) => {
+    // newElement.setAttribute(prop, props[prop]);
+    newElement[prop] = props[prop];
+  });
+
+  // Ajout des enfants
+  if (typeof children === "string") {
+    newElement.innerHTML = children;
+  } else if (typeof children === "object") {
+    render(children, newElement);
+  }
+
+  // Ajout de l'élément dans le DOM
+  element.appendChild(newElement);
 };
 
 const niveau1 = {
-    type: "a",
-    props: {
-        href: 'ynov.com',
-        style: 'color: #23b2a4; font-size: 40px',
-    },
-    children: `Lien vers ynov`,
-}
+  type: "a",
+  props: {
+    href: "http://ynov.com",
+    style: "color: #23b2a4; font-size: 40px",
+  },
+  children: `Lien vers ynov`,
+};
 
 const niveau2 = {
-    type: "div",
+  type: "div",
+  props: {
+    id: "container",
+    style: "padding: 10px 30px; background: #23b2a4; display: inline-block",
+  },
+  children: {
+    type: "a",
     props: {
-        id: 'container',
-        style: 'padding: 10px 30px; background: #23b2a4; display: inline-block',
+      href: "http://ynov.com",
+      target: "_blank",
+      class: "link",
+      style: "text-decoration: none",
     },
     children: {
-        type: "a",
-        props: {
-            href: 'http://ynov.com',
-            target: '_blank',
-            class: 'link',
-            style: 'text-decoration: none'
-        },
-        children: {
-            type: "p",
-            props: {
-                style: 'color: white; font-size: 25px',
-            },
-            children: 'YNOV',
-        },
+      type: "p",
+      props: {
+        style: "color: white; font-size: 25px",
+      },
+      children: "YNOV",
     },
-}
+  },
+};
 
-window.addEventListener('load', () => {
-    render(niveau1, "#root"); // une fois que ça fonctionne avec la variable "niveau1", remplacer par "niveau2"
+window.addEventListener("load", () => {
+  render(niveau2, "#root"); // une fois que ça fonctionne avec la variable "niveau1", remplacer par "niveau2"
 });
